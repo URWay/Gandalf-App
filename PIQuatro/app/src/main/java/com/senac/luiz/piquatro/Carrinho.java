ac4feafaa51b8965b9a479990925e5a8b52a458b
heyimleaf <heyimleaf@gmail.com>
2017-10-22 20:21:46 -0200

    Revert "Forma de pagamento"
    This reverts commit a8261dd7aad6e30b84cbe40d9f7a3ab23ba94aca.

--- PIQuatro/app/src/main/java/com/senac/luiz/piquatro/Carrinho.java
+++ PIQuatro/app/src/main/java/com/senac/luiz/piquatro/Carrinho.java
@@ -1,36 +1,13 @@
 package com.senac.luiz.piquatro;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.transition.TransitionManager;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.senac.luiz.piquatro.Carrinho;
 
 public class Carrinho extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrinho);
-
-
     }
 }
-
