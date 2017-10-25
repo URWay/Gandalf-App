6939e7778a3af1e894fe3b3ffc933eeb0a8990ef
heyimleaf <heyimleaf@gmail.com>
2017-10-25 20:25:39 -0200

    Checkout - Quase lá

--- PIQuatro/app/src/main/java/com/app/gandalf/piquatro/Home.java
+++ PIQuatro/app/src/main/java/com/app/gandalf/piquatro/Home.java
@@ -39,9 +39,6 @@ public class Home extends AppCompatActivity  implements NavigationView.OnNavigat
         setTitle(null);
 
 
-
-
-
         Intent intent = new Intent(this, CadastroCliente.class);
         startActivity(intent);
     }
@@ -61,16 +58,13 @@ public class Home extends AppCompatActivity  implements NavigationView.OnNavigat
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.home, menu);
         return false;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
---
+++ PIQuatro/app/src/main/java/com/app/gandalf/piquatro/StatusPedido.java
@@ -0,0 +1,33 @@
+package com.app.gandalf.piquatro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class StatusPedido extends AppCompatActivity {
+private ImageView imgstatus1;
+private ImageView imgstatus2;
+private ImageView imgstatus3;
+private TextView txtidpedido;
+private TextView txtstatus1;
+private TextView txtstatus2;
+private TextView txtstatus3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status_pedido);
+
+        imgstatus1 = (ImageView) findViewById(R.id.imgstatus1);
+        imgstatus2 = (ImageView) findViewById(R.id.imgstatus2);
+        imgstatus3 = (ImageView) findViewById(R.id.imgstatus3);
+
+        txtidpedido = (TextView) findViewById(R.id.txtidpedido);
+
+        txtstatus1 = (TextView) findViewById(R.id.txtstatus1);
+        txtstatus2 = (TextView) findViewById(R.id.txtstatus2);
+        txtstatus3 = (TextView) findViewById(R.id.txtstatus3);
+     }
+}
