71c243a2b9525306041a4e19a5d32c1ff88c3358
heyimleaf <heyimleaf@gmail.com>
2017-10-25 19:12:35 -0200

    View - Detalhes Produto

--- PIQuatro/app/src/main/java/com/app/gandalf/piquatro/CadastroEndereco.java
+++ PIQuatro/app/src/main/java/com/app/gandalf/piquatro/CadastroEndereco.java
@@ -20,10 +20,10 @@ private EditText txtendereco;
 private EditText txtnum;
 private EditText txtcep;
 private EditText txtcomplemento;
-private Spinner spinnercidade;
-private Spinner spinnerpais;
-private Spinner spinneruf;
- private Button btnok;
+//private Spinner spinnercidade;
+//private Spinner spinnerpais;
+//private Spinner spinneruf;
+private Button btnok;
 private EditText txtcidade;
 private EditText txtuf;
 private EditText txtpais;
@@ -42,83 +42,19 @@ private EditText txtpais;
         txtnum = (EditText) findViewById(R.id.txtnum);
         txtcep = (EditText) findViewById(R.id.txtcep);
         txtcomplemento = (EditText) findViewById(R.id.txtcomplemento);
-        spinnercidade = (Spinner) findViewById(R.id.spinnercidade);
-        spinnerpais = (Spinner) findViewById(R.id.spinnerpais);
-        spinneruf = (Spinner) findViewById(R.id.spinneruf);
+
         btnok = (Button) findViewById(R.id.btnok);
-
-        btnok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(txtnomeapelido.equals("") || txtendereco.equals("") || txtnum.equals("") ||
-                    txtcep.equals("") || spinnercidade != null && spinnercidade.getSelectedItem() != null ||
-                    spinnerpais != null && spinnerpais.getSelectedItem() != null ||
-                    spinneruf != null && spinneruf.getSelectedItem() != null){
-                    AlertDialog.Builder alerta = new AlertDialog.Builder(CadastroEndereco.this);
-                    alerta.setTitle("Erro!");
-                    alerta.setMessage("Os campos com * são obrigatórios!");
-
-                    alerta.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int id) {
-                            txtnomeapelido.setText("");
-                            txtendereco.setText("");
-                            txtnum.setText("");
-                            txtcep.setText("");
-
-                        }
-                    });
-                    AlertDialog dialog = alerta.create();
-                    dialog.show();
-
-                }
-            }
-
-        });
-
         txtcidade = (EditText) findViewById(R.id.txtcidade);
         txtuf = (EditText) findViewById(R.id.txtuf);
         txtpais = (EditText) findViewById(R.id.txtpais);
 
 
-        txtnum.addTextChangedListener(Mask.insert("######", txtnum));
-        txtcep.addTextChangedListener(Mask.insert("#####-###", txtcep));
+        //txtnum.addTextChangedListener(Mask.insert("######", txtnum));
+        //txtcep.addTextChangedListener(Mask.insert("#####-###", txtcep));
 
 
 
-
-        if(txtnomeapelido.equals("") || txtendereco.equals("") || txtnum.equals("") || txtcep.equals("") || txtcidade.equals("Selecione") || txtuf.equals("Selecione") || txtpais.equals("Selecione") ){
-            AlertDialog.Builder alerta = new AlertDialog.Builder(CadastroEndereco.this);
-            alerta.setTitle("Erro!");
-            alerta.setMessage("Preencha os campos corretamente!");
-            alerta.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int id) {
-                    txtnomeapelido.setText(null);
-                    txtendereco.setText(null);
-                    txtnum.setText(null);
-                    txtcep.setText(null);
-
-                }
-            });
-            AlertDialog dialog = alerta.create();
-            dialog.show();
-        }else{
-            AlertDialog.Builder alerta = new AlertDialog.Builder(CadastroEndereco.this);
-
-            alerta.setMessage("Cliente cadastrado com sucesso!");
-            alerta.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int id) {
-
-
-                }
-            });
-            AlertDialog dialog = alerta.create();
-            dialog.show();
-
-
-        }
 
     }
+
 }
